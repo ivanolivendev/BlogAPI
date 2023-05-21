c8176650a9fb43b0275a2e9222230c7734989b3a
Ivan Oliven <py.ivandeoliveira21@gmail.com>
2023-05-21 20:44:05 -0300

    Prévia 01
    Pesquisar sobre URLSearchParams. Está com um pequeno bug na hora de clicar no link que referencia o post. O programa não consegue pegar ele

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,41 @@
-console.log("teste")
+const url =  "https://jsonplaceholder.typicode.com/posts";
+const loadingElement = document.querySelector("#loading")
+const postContainer = document.querySelector("#post-container")
+
+
+//get id from url
+
+//const urlSearchParams = new urlSearchParams(window.location.search)
+//const postId = urlSearchParams.get("id")
+
+//Get all posts
+
+async function getAllposts (){
+    const response = await fetch(url)
+    console.log(response)
+    const data = await response.json();
+    console.log(data)
+
+    loadingElement.classList.add("hide");
+
+    data.map((post)=>{
+    const div = document.createElement("div");
+    const title = document.createElement("h2");
+    const body = document.createElement("p");
+    const link = document.createElement("a");
+
+    title.innerText=post.title;
+    body.innerText=post.body;
+    link.innerText='Ler artigo';
+    link.setAttribute("href",`/post.html?id=${post.id}`)
+
+    div.appendChild(title)
+    div.appendChild(body)
+    div.appendChild(link)
+
+    postContainer.appendChild(div )
+
+    })
+}
+
+getAllposts()
